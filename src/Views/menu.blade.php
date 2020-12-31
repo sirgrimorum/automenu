cd5235ebaab30e079268f0db2e240cf7cd4245bf
SirGrimorum <andres.espinosa@grimorum.com>
2020-12-31 13:10:54 -0500

    arreglar manejo de íconos

--- src/Views/menu.blade.php
+++ src/Views/menu.blade.php
@@ -12,7 +12,7 @@
     </a>
     @endif
     <button class="navbar-toggler {{\Illuminate\Support\Arr::get($config,"classes.button_izquierdo")}}" type="button" data-toggle="collapse" data-target="#navbar{{ucfirst($config['id'])}}" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fa {{\Illuminate\Support\Arr::get($config,"icons.left",'fa-bars')}}"></i>
+        <i class="{{\Illuminate\Support\Arr::get($config,"icons.left",'fa fa-bars')}}"></i>
     </button>
     <div class="collapse navbar-collapse {{\Illuminate\Support\Arr::get($config,"classes.navbar_collapse")}}" id="navbar{{ucfirst($config['id'])}}">
         @if($config["menu"]["izquierdo"]!="")
@@ -36,7 +36,7 @@
     <div class="dropdown">
         @if($config["menu"]["izquierdo"]!="")
         <button class="nav-item btn {{\Illuminate\Support\Arr::get($config,"classes.button_izquierdo")}}" type="button" id="navbar{{ucfirst($config['id'])}}_i_l" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa {{\Illuminate\Support\Arr::get($config,"icons.left",'fa-bars')}}"></i>
+            <i class="{{\Illuminate\Support\Arr::get($config,"icons.left",'fa fa-bars')}}"></i>
         </button>
         <div class="dropdown-menu {{\Illuminate\Support\Arr::get($config,"classes.navbar_collapse")}}" id="navbar{{ucfirst($config['id'])}}_i" aria-labelledby="navbar{{ucfirst($config['id'])}}_i_l">
             <div class="{{\Illuminate\Support\Arr::get($config,"classes.navbar_nav_izquierdo")}}" >
@@ -59,7 +59,7 @@
     <div class="dropdown">
         @if($config["menu"]["derecho"]!="")
         <button class="nav-item btn {{\Illuminate\Support\Arr::get($config,"classes.button_derecho")}}" type="button" id="navbar{{ucfirst($config['id'])}}_d_l" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa {{\Illuminate\Support\Arr::get($config,"icons.right",'fa-bars')}}"></i>
+            <i class="{{\Illuminate\Support\Arr::get($config,"icons.right",'fa fa-bars')}}"></i>
         </button>
         <div class="dropdown-menu {{\Illuminate\Support\Arr::get($config,"classes.navbar_collapse")}}" style="right: 0px;left: auto;" id="navbar{{ucfirst($config['id'])}}_d" aria-labelledby="navbar{{ucfirst($config['id'])}}_d_l">
             <div class="{{\Illuminate\Support\Arr::get($config,"classes.navbar_nav_derecho")}}">
